20addfd60655d8d7002230e416297f7b77316faa
ash03-g <letstalk1784@gmail.com>
2025-08-19 15:04:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
+provider "aws" {
+region = "ap-south-1"
+}
 
+resource "aws_instance" "one" {
+count = 10
+ami = "ami-0861f4e788f5069dd"
+instance_type = "t2.mciro"
+
+tags = {
+Name = "hcp-tet"
+}
+}
